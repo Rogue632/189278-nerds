8c6b78ce5055f7c9546f982b7ab797aeef734a28
Sergey Bedakov <rogue_87@mail.ru>
2016-09-08 22:52:56 +0300

    апдейт
    добавил анимацию форме,
    минимифицировал css и js

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,12 @@
 var button = document.querySelector(".contacts-btn");
 var popup = document.querySelector(".footer-contacts-popup");
 var close = document.querySelector(".popup-close")
+var login = popup.querySelector("[name=user-name]");
 
 button.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("footer-contacts-popup-show");
+  login.focus();
 });
 close.addEventListener("click", function(event) {
 	event.preventDefault();
